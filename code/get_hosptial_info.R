741280ad5d030154cbe7b7438265d0fb61259fce
Lewis Hooper <lewishooper@gmail.com>
2025-11-10 16:42:53 -0500

    backups mainly. Some new code but all helper stuff. not kety to product

---
+++ code/get_hosptial_info.R
@@ -0,0 +1,23 @@
+library(yaml)
+
+get_hospital_info <- function(fac) {
+  # Format FAC with leading zeros
+  fac_formatted <- sprintf("%03d", as.numeric(fac))
+  
+  # Load YAML
+  config <- read_yaml("enhanced_hospitals.yaml")
+  
+  # Find and return hospital info
+  for (hospital in config$hospitals) {
+    if (hospital$FAC == fac_formatted) {
+      return(list(
+        name = hospital$name,
+        url = hospital$url,
+        pattern = hospital$pattern
+      ))
+    }
+  }
+  
+  # Not found
+  return(NULL)
+}
